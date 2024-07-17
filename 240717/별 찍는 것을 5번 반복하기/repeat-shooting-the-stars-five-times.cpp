7cc93dae63f43d078624273c69413c0555e8f44f
MNA <66415336+mna11@users.noreply.github.com>
2024-07-17 19:11:08 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void Print5Starts(){
+    cout << "**********" << endl;
+}
+
+int main() {
+    for (int i = 0; i < 5; i++)
+        Print5Starts();
+    return 0;
+}
