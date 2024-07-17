e232f04b37159ca9fcb3cb8166a7613d335aaf67
MNA <66415336+mna11@users.noreply.github.com>
2024-07-17 19:13:35 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintStatement(int n){
+    for (int i = 0; i < n; i++){
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    PrintStatement(n);
+    return 0;
+}
