76d1aa26f34d7c62da8b4c0e167e8cc0422b656a
MNA <66415336+mna11@users.noreply.github.com>
2024-07-19 19:42:45 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 240719/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -5,8 +5,19 @@ using namespace std;
 
 string s1;
 string s2;
+
+bool SubFunc(int n){
+    for (int i = n; i < n + s2.length(); i++){
+        if (s1[i] != s2[i-n]) return false;
+    }
+    return true;
+}
+
 int Func(){
-    return s1.find(s2);
+    for (int i = 0; i < s1.length() - s2.length() + 1; i++){
+        if(SubFunc(i)) return i;
+    }
+    return -1;
 }
 
 int main() {
