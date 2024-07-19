fdcc232909ca7624b5520b8f749428a75b434680
MNA <66415336+mna11@users.noreply.github.com>
2024-07-19 19:24:50 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 240719/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -2,11 +2,13 @@
 #include <string>
 using namespace std;
 
-bool FindDuplicate(string &s){
-    while (s.length() >= 0){
-        char check = s[0];
+bool FindDiffer2Upper(string &s){
+    char check = s[0];
+    while (s.length() > 0){
+        if (check != s[0]) return true;
+
+        check = s[0];
         s.erase(0, 1);
-        if (s.find(check) != string::npos) return true;
     }
     return false;
 }
@@ -15,7 +17,7 @@ int main() {
     string s;
     cin >> s;
 
-    if (FindDuplicate(s)) cout << "Yes";
+    if (FindDiffer2Upper(s)) cout << "Yes";
     else cout << "No";
 
     return 0;
