a187d21a35d92deaec69109a2a980b8a02ea36de
MNA <66415336+mna11@users.noreply.github.com>
2024-07-19 19:26:25 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Func(int &a, int &b){
+    if (a > b) { a *= 2; b += 10; }
+    else { a += 10; b *= 2; }
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    Func(a, b);
+    cout << a << " " << b;
+    return 0;
+}
