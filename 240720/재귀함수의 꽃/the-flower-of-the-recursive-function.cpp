cca9ac7d61d816278a85162b5d7ede365d581913
MNA <66415336+mna11@users.noreply.github.com>
2024-07-20 13:30:08 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void PrintN(int n){
+    if (n == 0) return;
+    cout << "n ";
+    PrintN(n-1);
+    cout << "n ";
+    return;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    PrintN(n);
+    
+    return 0;
+}
