87494eda1c42a7674ca20899ad2f2820faaa6a39
MNA <66415336+mna11@users.noreply.github.com>
2024-07-20 14:19:29 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int MaxValArr(int arr[], int n, int len, int max){
+    if (n == len - 1) return max;
+    if (max < arr[n]) max = arr[n];
+    return MaxValArr(arr, n+1, len, max);
+}
+
+int main() {
+    int n;
+    int arr[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+    cout << MaxValArr(arr, 0, n, 0);
+    return 0;
+}
