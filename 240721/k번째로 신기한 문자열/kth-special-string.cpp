bbaf9e6ffda10d513159ff74a07d1b4dc4ef35ad
MNA <66415336+mna11@users.noreply.github.com>
2024-07-21 19:40:41 +0900

    Title: k번째로 신기한 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/k번째로 신기한 문자열/kth-special-string.cpp
+++ 240721/k번째로 신기한 문자열/kth-special-string.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-void OnlyStartT(string*& arr, string& T, int& n, int& cnt){
-    for (int i = 0; i < n; i++){
-        if (T.compare(arr[i].substr(0, T.length()))){
-            arr[i] = "1";
-            cnt++;
+void OnlyStartT(vector<string>& arr, string& T){
+    vector<string>::iterator iter;
+    for (iter = arr.begin(); iter != arr.end(); iter++){
+        if (T.compare((*iter).substr(0, T.length()))){
+            arr.erase(iter);
+            iter--;
         }
     }
-    sort(arr, arr + n);
+    sort(arr.begin(), arr.end());
 }
 
 int main() {
-    int n, k, cnt(0);
-    string T;
-    string* arr;
+    int n, k;
+    string T, tmp;
+    vector<string> arr;
 
     cin >> n >> k >> T;
+    arr.resize(n);
 
-    arr = new string[n];
     for (int i = 0; i < n; i++){
-        cin >> arr[i];
+        cin >> tmp;
+        arr.push_back(tmp);
     }
-    OnlyStartT(arr, T, n, cnt);
-    cout << arr[k+cnt-1];
+    OnlyStartT(arr, T);
+    cout << arr[k-1];
     return 0;
 }
