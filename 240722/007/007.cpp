f84a6111cae545d2b173041c0850626bedbb0737
MNA <66415336+mna11@users.noreply.github.com>
2024-07-22 19:22:22 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/007/007.cpp
+++ 240722/007/007.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
 using namespace std;
 
-class Spy{
-    public:
-        string s;
-        char c;
-        int n;
+// class Spy{
+//     public:
+//         string s;
+//         char c;
+//         int n;
 
-    Spy(string s, char c, int n) : s(s), c(c), n(n) {};
-};
+//     Spy(string s, char c, int n) : s(s), c(c), n(n) {};
+// };
 
-int main() {
+// int main() {
+//     string s;
+//     char c;
+//     int n;
+//     cin >> s >> c >> n;
+
+//     Spy spy(s, c, n);
+//     cout << "secret code : " << spy.s << endl;
+//     cout << "meeting point : " << spy.c << endl;
+//     cout << "time : " << spy.n << endl;
+
+//     return 0;
+// }
+
+int main(void){
     string s;
     char c;
-    int n;
-    cin >> s >> c >> n;
+    int t;
 
-    Spy spy(s, c, n);
-    cout << "secret code : " << spy.s << endl;
-    cout << "meeting point : " << spy.c << endl;
-    cout << "time : " << spy.n << endl;
+    cin >> s >> c >> t;
+    tuple<string, char, int> tup = make_tuple(s, c, t);
+    cout << "secret code : " << get<0>(tup) << endl;
+    cout << "meeting point : " << get<1>(tup) << endl;
+    cout << "time : " << get<2>(tup) << endl;
 
     return 0;
 }
