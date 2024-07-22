0661d29142aa1813f1dcfcba89354f903b8b77c8
MNA <66415336+mna11@users.noreply.github.com>
2024-07-22 20:38:14 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/비오는 날/rainy-day.cpp
+++ 240722/비오는 날/rainy-day.cpp
@@ -1,75 +1,47 @@
 #include <iostream>
 #include <string>
-#include <tuple>
 #include <algorithm>
 
 using namespace std;
 
-class Weather{
+class Rain{
 public:
-    int y, m, d;
-    string day;
-    string weather;
-    Weather() {};
-    Weather(tuple<int, int, int> t, string day_, string weather_) { 
-        tie(y, m, d) = t;
-        day = day_;
-        weather = weather_;
-    };
+    string y, m, d;
+    string days;
+    Rain(){};
+    Rain(string y, string m, string d, string days) : y(y), m(m), d(d), days(days) {};
 };
 
-tuple<int, int, int> DaySep(string s){
-    int point1(-1), point2(-1);
-    for (int i = 0; i < s.length(); i++){
-        if (s[i] == '-' && point1 == -1) point1 = i;
-        else if (s[i] == '-') point2 = i;
-    }
-    tuple<int, int, int> t;
-    t = make_tuple(stoi(s.substr(0, point1)), stoi(s.substr(point1 + 1, point2)), stoi(s.substr(point2+1, s.length())));
-    return t;
-}
-
-bool comp(const Weather& w1, const Weather& w2){
-    if (w1.y < w2.y) return true;
-    else if (w1.y == w2.y && w1.m < w2.m) return true;
-    else if (w1.y == w2.y && w1.m == w2.m && w1.d <= w2.d) return true;
+bool comp(const Rain& r1, const Rain& r2){
+    if (r1.y.compare(r2.y) < 0) return true;
+    else if (r1.y.compare(r2.y) == 0 && r1.m.compare(r2.m) < 0) return true;
+    else if (r1.y.compare(r2.y) == 0 && r1.m.compare(r2.m) == 0 && r1.d.compare(r2.d) < 0) return true;
     return false;
 }
 
 int main() {
     int n;
-    string s1, s2, s3;
-    Weather* w;
+    Rain r[100];
     
     cin >> n;
-    w = new Weather[n];
-    for (int i = 0 ; i < n; i++){
-        tuple<int, int, int> t;
-        cin >> s1 >> s2 >> s3;
-        t = DaySep(s1);
-        w[i] = Weather(t, s2, s3);
-    }
-
-    sort(w, w + n, comp);
-
     for (int i = 0; i < n; i++){
-        if (!w[i].weather.compare("Rain"))
-        {   
-            if (w[i].m < 10 && w[i].d < 10){
-            cout << w[i].y << "-0" << w[i].m << "-0" << w[i].d << " " << w[i].day << " " << w[i].weather << endl;        
-            }
-            else if (w[i].m < 10) {
-            cout << w[i].y << "-0" << w[i].m << "-" << w[i].d << " " << w[i].day << " " << w[i].weather << endl;    
-            }
-            else if (w[i].d < 10){
-            cout << w[i].y << "-" << w[i].m << "-0" << w[i].d << " " << w[i].day << " " << w[i].weather << endl;    
-            } else {
-            cout << w[i].y << "-" << w[i].m << "-" << w[i].d << " " << w[i].day << " " << w[i].weather << endl;
-            }
-            break;
+        string s1, s2, s3;
+        string year, month, day;
+        cin >> s1 >> s2 >> s3;
+        if (s3.compare("Rain")) {
+            i--;
+            n--;
+            continue;
         }
-    }
 
-    delete[] w;
+        year = s1.substr(0, 4);
+        month = s1.substr(5, 2);
+        day = s1.substr(8, 2);
+
+        r[i] = Rain(year, month, day, s2);
+    }
+    sort(r, r + n, comp);
+    cout << r[0].y << "-" << r[0].m << "-" << r[0].d << " " << r[0].days << " Rain";
+
     return 0;
 }
