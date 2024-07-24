cde79d121be37c38835fd7d60cb06c6ea72281ea
MNA <66415336+mna11@users.noreply.github.com>
2024-07-24 13:49:14 +0000

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <utility>
+#include <string>
+using namespace std;
+
+int Func(int m1, int m2, int d1, int d2, bool reverse){
+    int elapse_day(0);
+    int month_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    elapse_day = d2 - d1;
+    for (int i = m1; i < m2; i++){
+        elapse_day += month_day[i-1];
+    }
+
+    if (reverse) return 7 - (elapse_day % 7);
+    return elapse_day % 7;
+}
+
+int GuessDay(int m1, int m2, int d1, int d2){
+    pair<int, int> day1 = make_pair(m1, d1);
+    pair<int, int> day2 = make_pair(m2, d2);
+    if (day1 > day2) return Func(m2, m1, d2, d1, true);
+    else return Func(m1, m2, d1, d2, false);
+}
+
+int main() {
+    int m1, m2, d1, d2;
+    string day_of_week[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    cin >> m1 >> d1 >> m2 >> d2;
+    cout << day_of_week[GuessDay(m1, m2, d1, d2)];
+    return 0;
+}
