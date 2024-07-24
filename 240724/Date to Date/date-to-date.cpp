d1d96ed4a8c3eeb9625204981b535878737599a6
MNA <66415336+mna11@users.noreply.github.com>
2024-07-24 13:17:32 +0000

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/Date to Date/date-to-date.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Func(int m1, int d1, int m2, int d2){
+    int elapse_day(0);
+    int month_date[12] = {31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    elapse_day = d2 - d1;
+    for (int i = m1; i < m2; i++){
+        elapse_day += month_date[i];
+    }
+    return elapse_day;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cout << Func(m1, d1, m2, d2);
+    return 0;
+}
