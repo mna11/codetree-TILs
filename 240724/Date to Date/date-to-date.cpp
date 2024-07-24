b30b289e19ba1070ecf613007a2d90865a899d4c
MNA <66415336+mna11@users.noreply.github.com>
2024-07-24 13:22:35 +0000

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/Date to Date/date-to-date.cpp
+++ 240724/Date to Date/date-to-date.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int Func(int m1, int d1, int m2, int d2){
     int elapse_day(0);
-    int month_date[12] = {31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int month_date[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     elapse_day = d2 - d1 + 1;
     for (int i = m1; i < m2; i++){
         elapse_day += month_date[i-1];
