63a6872d9d62733399c1b0eae058fc0d5ed9bf64
MNA <66415336+mna11@users.noreply.github.com>
2024-07-28 22:06:06 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, cnt(0), max_cnt(0);
+    int arr[1001];
+
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        if (i == 0 || arr[i] != arr[i-1]) cnt = 0;
+        cnt++;
+        max_cnt = max(max_cnt, cnt);
+    }
+    cout << max_cnt;
+    return 0;
+}
