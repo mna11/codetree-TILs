72af49df9c7a850a5d4251a8be45ba396b3075de
MNA <66415336+mna11@users.noreply.github.com>
2024-07-28 22:09:26 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, cnt(0), max_cnt(0);
+    int arr[1001];
+
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        if (i == 0 || arr[i] > 0 != arr[i-1] > 0) cnt = 0;
+        max_cnt = max(max_cnt, ++cnt); 
+    }
+    cout << max_cnt;
+    return 0;
+}
