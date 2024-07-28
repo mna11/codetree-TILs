a3f8f3ab47bc1f5f1081eb89801cb8a08ac30844
MNA <66415336+mna11@users.noreply.github.com>
2024-07-28 22:42:44 +0900

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, k, result(-1);
+    int count[10001] = {0,};
+
+    cin >> n >> m >> k;
+    for (int i = 0; i < m; i++){
+        cin >> n;
+        count[n]++;
+        if (count[n] == 3) result = n;
+    }
+    cout << result;
+    return 0;
+}
