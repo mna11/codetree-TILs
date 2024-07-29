ce59e95ae74f3dfc466bb47d4a198708dc8036ad
MNA <66415336+mna11@users.noreply.github.com>
2024-07-29 22:43:08 +0900

    Title: 좌우로 움직이는 로봇, Time: 18ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240729/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#define A 0 
+#define B 1
+
+using namespace std;
+
+int main() {
+    int n, m;
+    int t;
+    char d;
+    int time(1);
+    int cord[2][1000001] = {{0,},};
+    bool is_same = false;
+    int result(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        cin >> t  >> d;
+        if (d == 'L') {
+            for (int j = 0 ; j < t; j++) {
+                cord[A][time] = cord[A][time-1] - 1;
+                time++;
+            }
+        }
+        else if (d == 'R'){
+            for (int j = 0; j < t; j++){
+                cord[A][time] = cord[A][time-1] + 1;
+                time++;
+            }
+        }
+    }
+    for (time; time < 1000001; time++) cord[A][time] = cord[A][time-1];
+    time = 1;
+    for (int i = 0; i < m; i++){
+        cin >> t  >> d;
+        if (d == 'L') {
+            for (int j = 0 ; j < t; j++) {
+                cord[B][time] = cord[B][time-1] - 1;
+                time++;
+            }
+        }
+        else if (d == 'R'){
+            for (int j = 0; j < t; j++){
+                cord[B][time] = cord[B][time-1] + 1;
+                time++;
+            }
+        }
+    }
+    for (time; time < 1000001; time++) cord[B][time] = cord[B][time-1];
+
+    for (int i = 1; i < 1000001; i++){
+        if (!is_same && cord[A][i] == cord[B][i]) is_same = true;
+        else if (is_same && cord[A][i] != cord[B][i]) {
+            result++;
+            is_same = false;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
