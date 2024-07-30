cfca86c2a5e5845f3ef88900c36a42e61d9ac6b5
MNA <66415336+mna11@users.noreply.github.com>
2024-07-30 22:30:06 +0900

    Title: 선두를 지켜라 3, Time: 12ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240730/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+#define A 0
+#define B 1
+using namespace std;
+
+int main() {
+    int n, m, v, t, time(1), max_time(-1);
+    int result(0);
+    bool is_same = true;
+    int road[2][1000001] = {{0,},};
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        cin >> v >> t;
+        for (int j = 0; j < t; j++) {
+            road[A][time] = road[A][time-1] + v;
+            time++;
+        }
+    }
+    max_time = time;
+    time = 1;
+    for (int i = 0; i < n; i++){
+        cin >> v >> t;
+        for (int j = 0; j < t; j++) {
+            road[B][time] = road[B][time-1] + v;
+            time++;
+        }
+    }
+    max_time = max(max_time, time);
+
+    for (int i = 1; i < max_time; i++){
+        if (!is_same && road[A][i] == road[B][i]) {
+            result++;
+            is_same = true;
+        }
+        else if (is_same && road[A][i] != road[B][i]) {
+            result++;
+            is_same = false; 
+        }
+    }
+    cout << result;
+    return 0;
+}
