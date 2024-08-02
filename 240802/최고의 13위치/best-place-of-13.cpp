dccb6299fccb089ccd1541597a50d61c91ce0205
MNA <66415336+mna11@users.noreply.github.com>
2024-08-02 22:32:35 +0900

    Title: 최고의 13위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+
+int main() {
+    vector<vector<int>> arr;
+    int n, max_coin(INT_MIN);
+
+    //init
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        vector<int> tmp;
+        for (int j = 0; j < n; j++){
+            int x;
+            cin >> x;
+            tmp.push_back(x);
+        }
+        arr.push_back(tmp);
+    }
+
+    //solve
+    for (int i = 0; i < n; i++){
+        int coin(0);
+        for (int j = 0; j < n - 2; j++){
+            coin = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+        }
+        max_coin = max(max_coin, coin);
+    }
+
+    //result
+    cout << max_coin;
+    return 0;
+}
