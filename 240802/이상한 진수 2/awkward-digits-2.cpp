bfa5ee7e72c0c41d2d5bb904ad78c8e6344c38fd
MNA <66415336+mna11@users.noreply.github.com>
2024-08-02 23:27:34 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/이상한 진수 2/awkward-digits-2.cpp
+++ 240802/이상한 진수 2/awkward-digits-2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -12,24 +14,45 @@ int BinToDec(string s){
     return dec / 2;
 }
 
-int main() {
-    bool has_zero = false;
-    string str;
-    int result;
-    cin >> str;
+// 0이 있을 경우, 최앞단 0을 1로 바꿔주고 출력 -> 최댓값
+// 만약 0이 없을 경우, 가장 마지막 부분을 0으로 바꿔주고 출력 -> 최댓값
+// int main() {
+//     bool has_zero = false;
+//     string str;
+//     int result;
+//     cin >> str;
     
+//     for (int i = 0; i < str.length(); i++){
+//         if (str[i] == '0') {
+//             str[i] = '1';
+//             result = BinToDec(str);
+//             has_zero = true;
+//             break;
+//         }
+//     }
+//     if (!has_zero) {
+//         str[str.length() - 1] = '0';
+//         result = BinToDec(str);
+//     }
+//     cout << result;
+//     return 0;
+// 
+
+
+// 완탐
+int main(void){
+    string str;
+    int max_dec(INT_MIN);
+    
+    cin >> str;
     for (int i = 0; i < str.length(); i++){
-        if (str[i] == '0') {
-            str[i] = '1';
-            result = BinToDec(str);
-            has_zero = true;
-            break;
-        }
+        int dec;
+
+        str[i] ^= 1;
+        dec = BinToDec(str);
+        max_dec = max(max_dec, dec);
+        str[i] ^= 1;
     }
-    if (!has_zero) {
-        str[str.length() - 1] = '0';
-        result = BinToDec(str);
-    }
-    cout << result;
-    return 0;
+
+    cout << max_dec;
 }
