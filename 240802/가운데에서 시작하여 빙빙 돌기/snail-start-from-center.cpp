4e608d03b663b03cdbb2a2636b66496cab96da01
MNA <66415336+mna11@users.noreply.github.com>
2024-08-02 19:32:46 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x, y, nx, ny, dir(0), distance(1);
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+    int arr[100][100] = {{0,},};
+
+    cin >> n;
+    x = y = n / 2;
+    arr[x][y] = 1;
+
+    for (int i = 2; i <= n*n;){
+        for (int j = 0; j < distance; j++){
+                x = x + dx[dir];
+                y = y + dy[dir];
+                arr[x][y] = i++;
+        }
+        dir = (dir + 3) % 4; // 반시계로 회전 
+        if (dir == 1 || dir == 3) continue;
+        else distance++;
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
