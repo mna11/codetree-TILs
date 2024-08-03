d8f6f812d08732ef3916c0e0250faa64fe2f09b5
MNA <66415336+mna11@users.noreply.github.com>
2024-08-03 22:43:02 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    int cnt(0);
+
+    cin >> str;
+
+    for (int i = 0; i < str.length() - 3; i++){ // ()) 자리 마련을 위해 - 3
+        for (int j = 2; j < str.length() - 1; j++) { // ) 자리 마련을 위해 - 1
+            string open_pare = str.substr(i, 2);
+            string close_pare = str.substr(j, 2);
+            if (!open_pare.compare("((") && !close_pare.compare("))")) cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
