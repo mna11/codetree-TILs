e02f534093fbaa729bbc24be234cb734852ac534
MNA <66415336+mna11@users.noreply.github.com>
2024-08-05 23:57:12 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240805/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240805/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -11,16 +11,22 @@ vector<int> arr;
 int main() {
     cin >> n >> k;
     arr.resize(101, 0);
+
     for (int i = 0; i < n; i++){
         int candy, basket_position;
         cin >> candy >> basket_position;
-        arr[basket_position] = candy;
+        arr[basket_position] += candy;
     }
 
-    for (int c = k + 1; c <= 100 - k; c++){
-        int sum(0);
-        for (int idx = c - k; idx <= c + k; idx++) sum += arr[idx];
-        max_sum = max(max_sum, sum);
+    if (k > 50) {
+        for (int i = 0; i <= 100; i++) max_sum += arr[i];
+    } 
+    else {
+        for (int c = k; c <= 100 - k; c++){
+            int sum(0);
+            for (int idx = c - k; idx <= c + k; idx++) sum += arr[idx];
+            max_sum = max(max_sum, sum);
+        }
     }
 
     cout << max_sum;
