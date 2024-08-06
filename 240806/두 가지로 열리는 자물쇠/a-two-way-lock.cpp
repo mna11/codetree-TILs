a95c808a033d1fa1398aa2f7c508d892b0316c55
MNA <66415336+mna11@users.noreply.github.com>
2024-08-06 23:52:48 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int a1, b1, c1;
+int a2, b2, c2;
+int cnt;
+
+bool IsCan(int x, int y){
+    return abs(x-y) <= 2 || n + (x < y ? x : y) - (x < y ? y : x) <= 2 ;
+}
+
+int main() {
+    cin >> n;
+    cin >> a1 >> b1 >> c1;
+    cin >> a2 >> b2 >> c2;
+    
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            for (int k = 1; k <= n; k++){
+                if (IsCan(i, a1) &&
+                    IsCan(j, b1) &&
+                    IsCan(k, c1)) cnt++;
+                else if (IsCan(i, a2) &&
+                         IsCan(j, b2) &&
+                         IsCan(k, c2)) cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
