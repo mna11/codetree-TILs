18df5cc180b344bbfb8cfbdc47f22e26c2abcbbc
MNA <66415336+mna11@users.noreply.github.com>
2024-08-08 23:42:35 +0900

    Title: 스승의 은혜 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> p;
+int n, b, cnt, budget;
+
+int main() {
+    cin >> n >> b;
+    for (int i = 0; i < n; i++){
+        int cost;
+        cin >> cost;
+        p.push_back(cost);
+    }
+
+    sort(p.begin(), p.end());
+    for (int i = 0; i < n; i++){
+        budget += p[i];
+        if (budget - p[i] / 2 > b){
+            cnt = i;
+            break;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
