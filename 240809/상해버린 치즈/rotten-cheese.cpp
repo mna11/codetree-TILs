e8f213e145dd2c0129f2105b19002b4dbf240113
MNA <66415336+mna11@users.noreply.github.com>
2024-08-09 23:03:08 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240809/상해버린 치즈/rotten-cheese.cpp
+++ 240809/상해버린 치즈/rotten-cheese.cpp
@@ -17,11 +17,11 @@ int m; // 몇번째 치즈
 int t; // 언제 먹었는지
 
 int eat_cheese[MAX_N + 1][MAX_M + 1] = {};
-int rotten_cheese_check[MAX_M + 1] = {};
+int rotten_cheese[MAX_M + 1] = {};
 int sick_time[MAX_N + 1] = {};
 bool sick_person[MAX_N + 1] = {};
 
-int eat_number, eat_time, rotten_cheese, max_check(INT_MIN), medicine;
+int eat_number, eat_time, max_check(INT_MIN), medicine;
 
 int main() {
 
@@ -29,6 +29,7 @@ int main() {
     cin >> N >> M >> D >> S;
     for (int i = 0; i < D; i++){
         cin >> p >> m >> t;
+        if (eat_cheese[p][m] != 0) continue;
         eat_cheese[p][m] = t; 
     }
     for (int i = 0; i < S; i++){
@@ -41,20 +42,20 @@ int main() {
         if (sick_time[i] == 0) continue;
         for (int j = 1; j <= M; j++){
             if (eat_cheese[i][j] == 0) continue;
-            if (sick_time[i] > eat_cheese[i][j]) rotten_cheese_check[j]++;
+            if (sick_time[i] > eat_cheese[i][j]) rotten_cheese[j]++;
         }
     }
 
     for (int i = 1; i <= M; i++){
-        if (max_check < rotten_cheese_check[i]) {
-            rotten_cheese = i;
-            max_check = rotten_cheese_check[i];
-        }
+        max_check = max(max_check, rotten_cheese[i]);
     }
 
     for (int i = 1; i <= N; i++){
         for (int j = 1; j <= M; j++){
-            if (eat_cheese[i][j] != 0 && j == rotten_cheese) medicine++;
+            if (eat_cheese[i][j] != 0 && rotten_cheese[j] == max_check) {
+                medicine++;
+                break;
+            }
         }
     }
 
