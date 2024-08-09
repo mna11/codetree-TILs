1c9452b66be2c7981052dafd443f351b06b9887f
MNA <66415336+mna11@users.noreply.github.com>
2024-08-09 23:54:21 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, B, P, S, buget(0), can_buy(0);
+vector<vector<int>> cost;
+
+
+bool compare(vector<int> c1, vector<int> c2){
+    if (c1[0] + c1[1] == c2[0] + c2[1]) return c1[0] < c2[0];
+    return c1[0] + c1[1] < c2[0] + c2[1];
+}
+
+int main() {
+
+    //init
+    cin >> N >> B;
+    for (int i = 0; i < N; i++){
+        vector<int> tmp;
+        cin >> P >> S;
+        tmp.push_back(P);
+        tmp.push_back(S);
+        cost.push_back(tmp);
+    }
+
+    //solve
+    sort(cost.begin(), cost.end(), compare);
+    for (int i = 0; i < N; i++){
+        P = cost[i][0];
+        S = cost[i][1];
+        buget += P + S;
+        if (buget - P / 2 > B) break;
+        can_buy++;
+    }
+    cout << can_buy;
+    return 0;
+}
