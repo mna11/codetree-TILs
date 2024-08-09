87ca5455ffef421c7c9a7f6024b373e733474022
MNA <66415336+mna11@users.noreply.github.com>
2024-08-09 23:36:41 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> booms;
+int N, K, max_number(INT_MIN);
+
+int main() {
+
+    //init
+    cin >> N >> K;
+    for (int i = 0; i < N; i++){
+        int number;
+        cin >> number;
+        booms.push_back(number);
+    }
+
+    //solve
+    for (int i = 0; i < N; i++){
+        for (int j = i + 1; j <= i + K; j++){
+            if (booms[i] != booms[j]) continue;
+            max_number = max(max_number, booms[i]);
+        }
+    }
+    cout << max_number;
+    return 0;
+}
