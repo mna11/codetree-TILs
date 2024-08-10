ee93bec4341113df179b316b1904416cde4016d6
MNA <66415336+mna11@users.noreply.github.com>
2024-08-10 23:34:01 +0900

    Title: 스승의 은혜 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240810/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240810/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -8,7 +8,6 @@ int N, B, P, S, buget(0), max_can_buy(0), can_buy(0);
 vector<vector<int>> cost;
 
 bool compare(vector<int> c1, vector<int> c2){
-    if (c1[0] + c1[1] == c2[0] + c2[1]) return c1[0] < c2[0];
     return c1[0] + c1[1] < c2[0] + c2[1];
 }
 
@@ -24,8 +23,6 @@ int main() {
         cost.push_back(tmp);
     }
 
-
-
     //solve
     sort(cost.begin(), cost.end(), compare);
     for (int i = 0; i < N; i++){
@@ -37,6 +34,7 @@ int main() {
             if (buget <= B) can_buy++;
             else buget -= (cost[j][0] + cost[j][1]);
         }
+        cout << endl;
         cost[i][0] *= 2;
         max_can_buy = max(max_can_buy, can_buy);
     }
