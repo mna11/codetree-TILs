e691cd6365ba81418d533ebf5c0b8d1efe46ae89
MNA <66415336+mna11@users.noreply.github.com>
2024-08-10 22:40:00 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240810/숫자 카운트/numeric-count.cpp
+++ 240810/숫자 카운트/numeric-count.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-#define MAX_NUMBER 10
+#define MAX_NUMBER 1000
 
 using namespace std;
 
-int n, strike, ball, cnt;
+int n(0), strike(0), ball(0), cnt(0);
 string num, tmp;
 bool is_possible_number[MAX_NUMBER + 1];
 
@@ -16,9 +16,11 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++){
         cin >> num >> strike >> ball;
+
         for (int j = 100; j <= 999; j++){
             int strike_cnt(0), ball_cnt(0);
             tmp = to_string(j);
+
             for (int d1 = 0; d1 < num.length(); d1++){
                 for (int d2 = 0; d2 < tmp.length(); d2++){
                     if (d1 == d2 && num[d1] == tmp[d2]) strike_cnt++;
@@ -30,10 +32,9 @@ int main() {
     }
 
     for (int i = 100; i <= 999; i++){
-        if (is_possible_number[i]) {
-            cnt++;
-        }
+        if (is_possible_number[i]) cnt++;
     }
+
     cout << cnt;
     return 0;
 }
