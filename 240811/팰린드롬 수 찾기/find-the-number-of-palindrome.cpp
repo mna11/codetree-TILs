2acacb411424b20c7029daf9ebe790e5e041a3ab
MNA <66415336+mna11@users.noreply.github.com>
2024-08-11 23:15:10 +0900

    Title: 팰린드롬 수 찾기, Time: 38ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
+++ 240811/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 int x, y, cnt;
-int digit_sep_number[7];
 
 int main() {
     cin >> x >> y;
     for (int i = x; i <= y; i++){
-        int digit(0);
         bool palindrome(true);
-
-        for (int j = i; j > 0; j /= 10) digit_sep_number[digit++] = j % 10;
-        for (int j = 0; j < digit; j++){
-            if (j > digit / 2) break;
-            if (digit_sep_number[j] != digit_sep_number[digit-j-1]) {
-                palindrome = false;
-                break;
-            }
-        }
+        string str1 = to_string(i);
+        string str2(str1);
+        reverse(str2.begin(), str2.end());
+        if (str1.compare(str2)) palindrome = false;
         if (palindrome) cnt++;
     }
     cout << cnt;
