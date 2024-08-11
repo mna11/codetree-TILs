e47932f5f0c6a2179d71f581a4c837459c99fd12
MNA <66415336+mna11@users.noreply.github.com>
2024-08-11 21:58:14 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/등차수열/arithmetic-sequence.cpp
+++ 240811/등차수열/arithmetic-sequence.cpp
@@ -18,19 +18,13 @@ int main() {
     }
 
     for (int k = 1; k <= 100; k++){
-        for (int i = 0; i < n - 1; i++){ // k의 위치: arr의 각 인덱스의 사이에 위치 
-            int cnt(0), differ = k - arr[i]; 
-            for (int j = 0; j < n - 1; j++){ // arr의 인덱스
-                if (i == j) cnt++;
-                else if (i == j + 1) {
-                    if(arr[j + 1] - k == differ) cnt++;
-                }
-                else {
-                    if (arr[j + 1] - arr[j] == differ) cnt++;
-                }
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++){ 
+            for (int j = i + 1; j < n; j++){
+                if (k - arr[i] == arr[j] - k) cnt++;
             }
-            max_cnt = max(max_cnt, cnt);
         }
+        max_cnt = max(max_cnt, cnt);
     }
     cout << max_cnt;
     return 0;
