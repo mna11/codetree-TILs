c30ef31ffa32dd860b9f1f23a4a76a5d45e9087f
MNA <66415336+mna11@users.noreply.github.com>
2024-08-11 20:42:29 +0900

    Title: 정보에 따른 숫자 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/정보에 따른 숫자 2/number-based-on-information-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+#define MAX_X 1000
+
+
+using namespace std;
+
+
+char line[MAX_X + 1];
+int t, a, b, cnt;
+
+int main() {
+    cin >> t >> a >> b;
+    for (int i = 0; i < t; i++){
+        char alphar;
+        int x;
+        cin >> alphar >> x;
+        line[x] = alphar;
+    }
+
+    for (int i = a; i <= b; i++){
+        int d1(INT_MAX), d2(INT_MAX);
+        for (int j = 0; j <= 1000; j++){
+            if (line[j] == 'S') d1 = min(d1, abs(i-j));
+            else if (line[j] == 'N') d2 = min(d2, abs(i-j));
+        }
+        if (d1 <= d2) cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
