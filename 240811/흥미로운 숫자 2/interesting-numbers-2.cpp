3b0ed573d7af8a754cc2327ef1f4c02695088f30
MNA <66415336+mna11@users.noreply.github.com>
2024-08-11 20:18:43 +0900

    Title: 흥미로운 숫자 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int x, y, ans;
+int digit_sep_number[7] = {0,}; 
+
+int main() {
+    cin >> x >> y;
+    for (int i = x; i <= y; i++){
+        int same(0), digit(0);
+        int check[10] = {0,};
+
+        for (int j = i; j > 0; j/=10) digit_sep_number[digit++] = j % 10;
+        for (int j = 0; j < digit; j++) check[digit_sep_number[j]]++;
+        sort(check, check + digit, greater<int>());
+        if (check[0] == digit - 1) ans++;
+    }
+    cout << ans;
+    return 0;
+}
