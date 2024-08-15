eea313c09839287e43736dff4573c39c45ba10aa
MNA <66415336+mna11@users.noreply.github.com>
2024-08-15 23:00:24 +0900

    Title: 가장 많이 나온 쌍, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/가장 많이 나온 쌍/the-most-frequent-pair.cpp
+++ 240815/가장 많이 나온 쌍/the-most-frequent-pair.cpp
@@ -14,13 +14,14 @@ int main() {
     for (int i = 0; i < m; i++) cin >> input[i][0] >> input[i][1];
 
     for (int a = 1; a <= n; a++){
-        int cnt(0);
         for (int b = 1; b <= n; b++){
+            int cnt(0);
             for (int i = 0; i < m; i++){
-                if (a == input[i][0] && b == input[i][1]) cnt++;
+                if ((a == input[i][0] && b == input[i][1]) ||
+                    (a == input[i][1] && b == input[i][0])) cnt++;
             }
+            max_cnt = max(max_cnt, cnt);
         }
-        max_cnt = max(max_cnt, cnt);
     }
     cout << max_cnt;
     return 0;
