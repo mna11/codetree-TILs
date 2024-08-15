b7598ab147242e229d21d53443d2053eebd5b886
MNA <66415336+mna11@users.noreply.github.com>
2024-08-15 22:58:30 +0900

    Title: 가장 많이 나온 쌍, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/가장 많이 나온 쌍/the-most-frequent-pair.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_M 100
+
+using namespace std;
+
+int input[MAX_M][2] = {{0,},};
+int n, m, max_cnt(INT_MIN);
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) cin >> input[i][0] >> input[i][1];
+
+    for (int a = 1; a <= n; a++){
+        int cnt(0);
+        for (int b = 1; b <= n; b++){
+            for (int i = 0; i < m; i++){
+                if (a == input[i][0] && b == input[i][1]) cnt++;
+            }
+        }
+        max_cnt = max(max_cnt, cnt);
+    }
+    cout << max_cnt;
+    return 0;
+}
