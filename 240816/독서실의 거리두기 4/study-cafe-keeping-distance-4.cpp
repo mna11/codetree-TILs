ce7236ccb370c9f3b17ca86f99ebdd85dcd3ba16
MNA <66415336+mna11@users.noreply.github.com>
2024-08-16 23:47:04 +0900

    Title: 독서실의 거리두기 4, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int sit[MAX_N], min_dist(INT_MAX), ans(INT_MIN);
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) scanf("%1d", &sit[i]);
+
+    for (int s1 = 0; s1 < n; s1++){
+        if (sit[s1] == 1) continue;
+        for (int s2 = s1 + 1; s2 < n; s2++){
+            if (sit[s2] == 1) continue;
+
+            sit[s1] = sit[s2] = 1;
+
+            min_dist = INT_MAX;
+            for (int i = 0; i < n; i++){
+                if (sit[i] == 0) continue;
+                for (int j = i + 1; j < n; j++){
+                    if (sit[i] == 1 && sit[j] == 1){
+                        int dist = j - i;
+                        min_dist = min(min_dist, dist);
+                        break;
+                    }
+                }
+            }
+           
+            if(min_dist != INT_MAX) ans = max(ans, min_dist);
+            sit[s1] = sit[s2] = 0;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
