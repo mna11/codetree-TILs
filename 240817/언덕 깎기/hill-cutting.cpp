52dc2638cc6be570f44ad1870a231ceebcce3986
MNA <66415336+mna11@users.noreply.github.com>
2024-08-17 23:58:31 +0900

    Title: 언덕 깎기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240817/언덕 깎기/hill-cutting.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n, cost, up_down(1);
+int hill[MAX_N];
+int tmp[MAX_N];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> hill[i];
+    
+    sort(hill, hill + n);
+    copy(begin(hill), end(hill), begin(tmp));
+    
+    while(tmp[n-1] - tmp[0] > 17){
+        up_down %= 2;
+        if (up_down == 1){
+            tmp[0]++;
+        }
+        else {
+            tmp[n-1]--;
+        }
+        up_down++;
+        sort(tmp, tmp+n);
+    }
+    
+    for (int i = 0; i < n; i++){
+        cost += abs(tmp[i] - hill[i]) * abs(tmp[i] - hill[i]);
+    }
+    cout << cost;
+    return 0;
+}
