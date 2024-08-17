390ce37d4759f1338bd529932fa61607f0f36da5
MNA <66415336+mna11@users.noreply.github.com>
2024-08-17 22:57:58 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240817/이상한 폭탄 3/strange-bomb-3.cpp
+++ 240817/이상한 폭탄 3/strange-bomb-3.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int n, k, cnt, ans;
 int line[MAX_N + 1];
-bool is_boom[MAX_N + 1];
+bool is_boom[MAX_N + 1], boom_check;
 int number_cnt[MAX_NUMBER + 1];
 
 int main() {
@@ -18,13 +18,16 @@ int main() {
         cin >> line[i];
     }
 
-    for (int i = 1; i <= n; i++){
-        for (int j = i + 1; j <= n + k; j++){
+    for (int i = 1; i <= n - k; i++){
+        for (int j = i + 1; j <= i + k; j++){
             if (line[i] == line[j]){
                 is_boom[i] = is_boom[j] = true;
+                boom_check = true;
             }
         }
     }
+    
+    //for (int i = 0; i <= n; i++) cout << i << " " << is_boom[i] << endl;
 
     for (int i = 1; i <= n; i++){
         if (is_boom[i]) number_cnt[line[i]]++;
@@ -33,7 +36,7 @@ int main() {
             ans = line[i];
         }
     }
-
+    if (!boom_check) ans = 0;
     cout << ans;
 
     return 0;
