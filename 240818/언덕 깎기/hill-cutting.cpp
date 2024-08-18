ff8b8428e89f85454b0bf67fe39e7540a2093323
MNA <66415336+mna11@users.noreply.github.com>
2024-08-18 23:05:23 +0900

    Title: 언덕 깎기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240818/언덕 깎기/hill-cutting.cpp
+++ 240818/언덕 깎기/hill-cutting.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 #include <cmath>
 
 #define MAX_N 1000
 
 using namespace std;
 
-int n, cost, up_down;
+int n, cost, min_cost(INT_MAX), min_hill, max_hill;
 int hill[MAX_N];
 int tmp[MAX_N];
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) cin >> hill[i];
-    
     sort(hill, hill + n);
-    copy(begin(hill), end(hill), begin(tmp));
-    
-    while(tmp[n-1] - tmp[0] > 17){
-        if (up_down % 2 == 0){
-            tmp[0]++;
+    min_hill = hill[0];
+    max_hill = hill[0] + 17;
+
+    while(max_hill <= hill[n-1]){
+        cost = 0;
+        for (int i = 0; i < n; i++){
+            if (hill[i] >= min_hill && hill[i] <= max_hill) continue;
+            if (abs(hill[i] - min_hill) < abs(hill[i] - max_hill)) {
+                cost += abs(hill[i] - min_hill) * abs(hill[i] - min_hill);
+            }
+            else {
+                cost += abs(hill[i] - max_hill) * abs(hill[i] - max_hill);
+            }
         }
-        else {
-            tmp[n-1]--;
-        }
-        up_down++;
-        sort(tmp, tmp+n);
+        min_cost = min(min_cost, cost);
+        min_hill++;
+        max_hill++;
     }
+
+    cout << min_cost;
     
-    for (int i = 0; i < n; i++){
-        cost += abs(tmp[i] - hill[i]) * abs(tmp[i] - hill[i]);
-    }
-    cout << cost;
+
     return 0;
 }
