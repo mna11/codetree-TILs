8256716ba1b59434451e5af3fc205efd8d1c3baa
MNA <66415336+mna11@users.noreply.github.com>
2024-08-19 23:23:59 +0900

    Title: 전부 겹치는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 240819/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -2,26 +2,33 @@
 #include <algorithm>
 #include <climits>
 
+#define MAX_N 100
+
 using namespace std;
 
 int n;
-int x1, x2, min_x2(INT_MAX), max_x1(INT_MIN), out_range(0);
+int x1[MAX_N], x2[MAX_N]; 
+int min_x2(INT_MAX), max_x1(INT_MIN);
+int is_that_all_overlap(true), can_overlap(false);
 
 int main() {
     cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> x1[i] >> x2[i];
     for (int i = 0; i < n; i++){
-        cin >> x1 >> x2;
-        int tmp_max_x1 = max(max_x1, x1);
-        int tmp_min_x2 = min(min_x2, x2);
-
-        if (tmp_max_x1 > tmp_min_x2) out_range++;
-        else {
-            max_x1 = tmp_max_x1;
-            min_x2 = tmp_min_x2;
+        min_x2 = INT_MAX;
+        max_x1 = INT_MIN;
+        is_that_all_overlap = true;
+        for (int j = 0; j < n; j++){
+            if (i == j) continue;
+            max_x1 = max(max_x1, x1[j]);
+            min_x2 = min(min_x2, x2[j]);
+            if (max_x1 > min_x2) is_that_all_overlap = false;
         }
+        if(is_that_all_overlap) can_overlap = true;
     }
-
-    if (out_range <= 1 || out_range == n - 1) cout << "Yes";
+    if (can_overlap) cout << "Yes";
     else cout << "No";
+
     return 0;
 }
