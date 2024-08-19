daa1bb6d406d1d5c8d846b5d4c147fb927edb49b
MNA <66415336+mna11@users.noreply.github.com>
2024-08-19 23:09:50 +0900

    Title: 전부 겹치는 선분 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240819/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 240819/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -15,12 +15,15 @@ int main() {
         cin >> x1 >> x2;
         int tmp_max_x1 = max(max_x1, x1);
         int tmp_min_x2 = min(min_x2, x2);
-        if (out_range == 0 && tmp_max_x1 < tmp_min_x2) out_range++;
+
+        if (tmp_max_x1 < tmp_min_x2) out_range++;
         else {
             max_x1 = tmp_max_x1;
             min_x2 = tmp_min_x2;
         }
+        if (out_range == 2) break;
     }
+
     if (out_range <= 1) cout << "Yes";
     else cout << "No";
     return 0;
