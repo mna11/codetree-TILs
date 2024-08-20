dbf3b9e7027a3d623c35c47d7123aa1207e13590
MNA <66415336+mna11@users.noreply.github.com>
2024-08-20 23:49:51 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240820/코딩톡/coding-talk.cpp
+++ 240820/코딩톡/coding-talk.cpp
@@ -8,7 +8,8 @@ using namespace std;
 int n, m, p, u;
 char c;
 int send_person[MAX_M], unreading_person_cnt[MAX_M];
-array<array<bool, MAX_N>, MAX_M> check_reading_person;
+array<array<bool, MAX_N>, MAX_M> check_unreading_person;
+array<int, MAX_M> can_unreading_person_cnt;
 
 int main() {
     cin >> n >> m >> p;
@@ -16,16 +17,41 @@ int main() {
         cin >> c >> u;
         send_person[i] = c;
         unreading_person_cnt[i] = u;
+        if (u == 0) check_unreading_person[i].fill(false);
+        else check_unreading_person[i].fill(true);
     }
 
     for (int i = 0; i < m; i++){
-        check_reading_person[i].fill(false);
+        can_unreading_person_cnt[i] = n;
         for (int j = i; j < m; j++){
-            check_reading_person[i][send_person[j] - 'A'] = true;
+            if (check_unreading_person[i][send_person[j] - 'A']) {
+                can_unreading_person_cnt[i]--;
+                check_unreading_person[i][send_person[j] - 'A'] = false;
+            }
         }
     }
+
+
+    // for (int i = 0; i < m; i++){
+    //     cout << can_unreading_person_cnt[i] << " ";
+    //     for (int j = 0; j < n; j++){
+    //         if(check_unreading_person[i][j]) cout << char(j + 'A') << " "; 
+    //     }
+    //     cout << endl;
+    //}
+    
+    for (int i = m - 1; i >= 0; i--){
+        if (can_unreading_person_cnt[i] == unreading_person_cnt[i]){
+            for (int j = i - 1; j >= 0; j--){
+                for (int k = 0; k < n; k++){
+                    if (check_unreading_person[i][k]) check_unreading_person[j][k] = true;
+                }
+            }
+        }
+    }
+
     for (int i = 0; i < n; i++){
-        if(!check_reading_person[p - 1][i]) cout << char(i + 'A') << " ";
+        if(check_unreading_person[p - 1][i]) cout << char(i + 'A') << " ";
     }
     return 0;
 }
