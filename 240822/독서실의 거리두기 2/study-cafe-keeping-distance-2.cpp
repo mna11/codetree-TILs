63e2fd5d3feb69a8a563b737883e80cbfc8e501c
MNA <66415336+mna11@users.noreply.github.com>
2024-08-22 23:19:15 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_N 1000
+using namespace std;
+
+int n, length, max_length(INT_MIN), min_length(INT_MAX), seat_add_index;
+int seat[MAX_N];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) scanf("%1d", &seat[i]);
+
+    for (int i = 0; i < n; i++){
+        if (seat[i] == 0) continue;
+        for (int j = i + 1; j < n; j++){
+            if (seat[j] == 1) {
+                length = j - i;
+                if (max_length < length){
+                    max_length = length;
+                    seat_add_index = (i + j) / 2;
+                }
+                break;
+            }
+
+            if (j == n-1){
+                length = j - i;
+                if (max_length / 2 < length){
+                    max_length = length;
+                    seat_add_index = j;
+                }
+            }
+        }    
+    }
+    seat[seat_add_index] = 1;
+
+    for (int i = 0; i < n; i++){
+        if (seat[i] == 0) continue;
+        for (int j = i + 1; j < n; j++){
+            if (seat[j] == 0) continue;
+            
+            if (seat[j] == 1){
+                length = j- i;
+                min_length = min(min_length, length);
+                break;
+            }
+        }
+    }
+
+    cout << min_length;
+    return 0;
+}
