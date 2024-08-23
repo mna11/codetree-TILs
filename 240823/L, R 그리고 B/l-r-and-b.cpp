70d62a11c807ac9fcaf9af61281de56f486c39cf
MNA <66415336+mna11@users.noreply.github.com>
2024-08-23 23:01:48 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240823/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <utility>
+#include <cmath>
+
+using namespace std;
+
+pair<int, int> L;
+pair<int, int> R;
+pair<int, int> B;
+char input;
+int ans;
+
+int main() {
+    for (int y = 1; y <= 10; y++){
+        for (int x = 1; x <= 10; x++){
+            cin >> input;
+            if (input == '.') continue;
+            else if (input == 'L') L = make_pair(y, x);
+            else if (input == 'R') R = make_pair(y, x);
+            else if (input == 'B') B = make_pair(y, x);
+        }
+    }
+
+    if (L.first == R.first && R.first == B.first) {
+        ans = abs(L.second - B.second) + 1;
+    }
+    else if (L.second == R.second && R.second == B.second){
+        ans = abs(L.first - B.first) + 1;
+    }
+    else {
+        ans = abs(L.first - B.first) + abs(L.second - B.second) - 1;
+    }
+
+    cout << ans;
+    return 0;
+}
