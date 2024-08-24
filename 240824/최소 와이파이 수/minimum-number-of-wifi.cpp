53f8947f3ae8818b49a758f0e8a4bd00d56eb029
MNA <66415336+mna11@users.noreply.github.com>
2024-08-24 22:37:27 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240824/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, wifi_cnt, cur_pos, nxt_pos;
+vector<int> houses;
+
+bool CheckRangeInPerson(int s){
+    for (int i = s - m; i <= s + m; i++){
+        if ( i < 0 || i >= n) continue;
+        
+        if (houses[i] == 1) return true;
+    }
+    return false;
+}
+
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        houses.push_back(x);
+    }
+
+    cur_pos = 0 + m;
+    nxt_pos = cur_pos + m + 1 + m;
+    if (cur_pos >= n){
+        if (CheckRangeInPerson(cur_pos)) wifi_cnt = 1;
+    }
+    while(cur_pos < n + m){
+        if (CheckRangeInPerson(cur_pos)) wifi_cnt++;
+        cur_pos = nxt_pos;
+        nxt_pos = cur_pos + m + 1 + m;
+    }
+    cout << wifi_cnt;
+    return 0;
+}
