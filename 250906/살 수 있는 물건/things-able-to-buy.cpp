9fe8c74615fb7cd90693b51e5faad0196fc12390
MNA11 <thkim306@gmail.com>
2025-09-06 11:35:04 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250906/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money;
+    cin >> money;
+    if (money >= 3000) cout << "book";
+    else if (money >= 1000) cout << "mask";
+    else cout << "no";
+    return 0;
+}
