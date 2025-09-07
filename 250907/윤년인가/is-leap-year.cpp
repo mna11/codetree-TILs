cdb92d862aeb6f89ad85943d0db3e89d9b14d2af
MNA11 <thkim306@gmail.com>
2025-09-07 09:56:17 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250907/윤년인가/is-leap-year.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Y;
+    cin >> Y;
+    if ((Y % 4 == 0 && Y % 100 != 0) || (Y % 400 == 0)) cout << "true";
+    else cout << "false";
+    return 0;
+}
