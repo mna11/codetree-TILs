6a5d2b27bd7dc531fc2f55873d181170a10b6777
MNA11 <thkim306@gmail.com>
2025-09-11 09:59:08 +0900

    Title: 실수와 문자 받아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+    cin >> c >> a >> b;
+    printf("%c\n%.2f\n%.2f", c, a, b);
+    return 0;
+}
