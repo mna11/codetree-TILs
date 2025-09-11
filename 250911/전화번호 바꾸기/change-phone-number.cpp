d894f9be9988c3be16b2f3714f540c46ea71b9fb
MNA11 <thkim306@gmail.com>
2025-09-11 10:08:36 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, f, s;
+    cin >> n;
+    cin.get();
+    cin >> f;
+    cin.get();
+    cin >> s;
+    printf("010-%d-%d",s,f);
+    return 0;
+}
