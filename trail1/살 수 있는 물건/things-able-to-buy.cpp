1330d1ec03b843c958129c9f3e7bf74073cfaddb
MNA11 <thkim306@gmail.com>
2026-07-01 08:12:47 +0900

    [Passed] 살 수 있는 물건 (2ms, 0MB)

---
+++ trail1/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N >= 3000) cout << "book";
+    else if (N >= 1000) cout << "mask";
+    else cout << "no";
+    return 0;
+}
