645944e10b5932c6a4d505d87356d46b3d063531
MNA11 <thkim306@gmail.com>
2026-07-07 10:16:54 +0900

    [Passed] 세 수의 중간값 (2ms, 0MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    if ( B > A && B < C) cout << 1 << endl;
+    else cout << 0 << endl;
+    return 0;
+}
