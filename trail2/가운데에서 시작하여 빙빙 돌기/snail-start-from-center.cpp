34b70e50c58aca9a08f6ec5f4d1baf01433d4b5b
MNA11 <thkim306@gmail.com>
2026-07-16 10:54:39 +0900

    [Passed] 가운데에서 시작하여 빙빙 돌기 (1ms, 0MB)

---
+++ trail2/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+bool InRange(int N, int x, int y)
+{
+    return (x >= 0 && x < N && y >= 0 && y < N);
+}
+
+//
+// 1 1 2 2 3 3 4 4 5 5 6 6
+// 2 4 6 8 10 
+
+int main(void) {
+    int N(0);
+    int dx[4] = { 1, 0 , -1, 0 };
+    int dy[4] = { 0, -1, 0, 1 };
+    int dir(0);
+
+    int cnt(0);
+    int max_cur_cnt(1);
+    int turn_cnt(0);
+    int iMatrix[100][100];
+
+    cin >> N;
+
+    int iTmpCx(0), iTmpCy(0);
+    int cx(N / 2), cy(N / 2);
+    iMatrix[cy][cx] = 1;
+    for (int i = 2; i <= N * N; ++i) {
+        iTmpCx = cx + dx[dir];
+        iTmpCy = cy + dy[dir];
+
+        if ((!InRange(N, iTmpCx, iTmpCy)) || cnt == max_cur_cnt) {
+            dir = (dir + 1) % 4;
+            ++turn_cnt;
+            if (2 == turn_cnt) {
+                turn_cnt = 0;
+                ++max_cur_cnt;
+            }
+            cnt = 0;
+        }
+
+        cx += dx[dir];
+        cy += dy[dir];
+        iMatrix[cy][cx] = i;
+        ++cnt;
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cout << iMatrix[i][j] << '\t';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
