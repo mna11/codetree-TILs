c8727c299109ac368a885f8adcb972d5f3b09363
MNA11 <thkim306@gmail.com>
2026-07-16 10:10:03 +0900

    [Passed] 빙빙 돌며 사각형 채우기 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+
+bool InRange(int N, int M, int x, int y)
+{
+    return (x >= 0 && x < M && y >= 0 && y < N);
+}
+
+int main() {
+    int N(0), M(0);
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    int dir(0);
+    int cx(0), cy(0);
+    int iMatrix[100][100] = {{-1,},};
+
+    cin >> N >> M;
+
+    int iTmpCx(0), iTmpCy(0);
+    iMatrix[0][0] = 1;
+    for (int i = 2; i <= N*M; ++i) {
+        iTmpCx = cx + dx[dir];
+        iTmpCy = cy + dy[dir];
+        
+        if ((!InRange(N, M, iTmpCx, iTmpCy))
+        || 0 != iMatrix[iTmpCy][iTmpCx])
+            dir = (dir + 1) % 4;
+        
+        cx += dx[dir];
+        cy += dy[dir];
+
+        iMatrix[cy][cx] = i;
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            cout << char((iMatrix[i][j] - 1) % 26 + 'A') << ' ';  
+        }
+        cout << endl;
+    }
+    return 0;
+}
