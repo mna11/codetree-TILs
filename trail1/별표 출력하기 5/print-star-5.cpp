8ba9451193b8bb5ffc204c4aaf78e98d14b028ce
Taehun KIM <thkim306@gmail.com>
2026-08-06 09:27:22 +0900

    [Passed] 별표 출력하기 5 (2ms, 0MB)

---
+++ trail1/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N(0);
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N - i; ++j)
+        {
+            for (int l = 0; l < N - i; ++l)
+            {
+                cout << "*";
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
