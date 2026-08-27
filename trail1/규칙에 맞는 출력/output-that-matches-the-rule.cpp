8eaf0613669b7b371ecd277606a1b3f2f39fb019
Taehun KIM <thkim306@gmail.com>
2026-08-27 09:54:03 +0900

    [Passed] 규칙에 맞는 출력 (2ms, 0MB)

---
+++ trail1/규칙에 맞는 출력/output-that-matches-the-rule.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N(0);
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = N - i; j <= N; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
