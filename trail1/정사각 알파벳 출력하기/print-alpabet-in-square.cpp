d69c361b9309056d6227c73ca676f0454676cb2d
Taehun KIM <thkim306@gmail.com>
2026-09-05 22:21:58 +0900

    [Passed] 정사각 알파벳 출력하기 (2ms, 0MB)

---
+++ trail1/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N(0);
+    cin >> N;
+    int iCnt(0);
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cout << (char)('A' + iCnt++);
+        }
+        cout << endl;
+    }
+    return 0;
+}
